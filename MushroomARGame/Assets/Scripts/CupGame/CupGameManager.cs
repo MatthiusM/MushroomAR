5ede2dd32b3a98225dd9d5bc5e29b92a9f9e397d
Matthius Mlengah <91685851+MatthiusM@users.noreply.github.com>
2024-02-10 10:43:43 +0000

    add start sequence into play round

--- MushroomARGame/Assets/Scripts/CupGame/CupGameManager.cs
+++ MushroomARGame/Assets/Scripts/CupGame/CupGameManager.cs
@@ -1,15 +1,54 @@
 using System;
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CupGameManager : MonoBehaviour
 {
+    public enum GameState
+    {
+        Starting,
+        Playing,
+        Picking,
+        Ending
+    }
+
+    public GameState CurrentState { get; private set; } = GameState.Starting;
+
     public Action onGameStart;
-    public Action onPlayRound;
+    public Action<int, float> onPlayRound;
     public Action onGameEnd;
 
+    private CupGame cupGame;
+
+    private void Awake()
+    {
+        DebugUtility.TryGetComponentWithErrorLog<CupGame>(gameObject, out cupGame);               
+    }
+
     private void Start()
-    {        
+    {
         onGameStart?.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        cupGame.onStartFinished += StartFinished;
+    }
+    
+    private void OnDisable()
+    {
+        cupGame.onStartFinished -= StartFinished;
+    }
+
+    private void StartFinished()
+    {
+        StartCoroutine(Waitswconds(1f));
+        onPlayRound?.Invoke(UnityEngine.Random.Range(6, 9), 0.5f);
+    }
+
+    IEnumerator Waitswconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
     }
 }
