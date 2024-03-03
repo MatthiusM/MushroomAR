695872b157cf906876c0a3c584de730ffe96e6f4
Matthius Mlengah <91685851+MatthiusM@users.noreply.github.com>
2024-03-03 10:16:16 +0000

    place mini game with new input system

--- MushroomARGame/Assets/Scripts/PlaceOnPlane.cs
+++ MushroomARGame/Assets/Scripts/PlaceOnPlane.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem.EnhancedTouch;
 using UnityEngine.XR.ARFoundation;
 
 //www.youtube.com. (n.d.). Unity AR Tutorial: AR Raycast - place an object mutiple times. [online] Available at: https://www.youtube.com/watch?v=pC3146FjNC0
@@ -15,22 +17,31 @@ public class PlaceOnPlane : MonoBehaviour
 
     bool placed = false;
 
-    void Update()
+    private void OnEnable()
     {
-        if (Input.touchCount == 0 || placed)
+        InputManager.Instance.OnTapEvent += PlaceMiniGame;
+    }
+
+    private void OnDisable()
+    {
+        InputManager.Instance.OnTapEvent -= PlaceMiniGame;
+    }
+
+    private void PlaceMiniGame(Vector2 inputPosition)
+    {
+        if (placed)
             return;
 
-        Touch touch = Input.GetTouch(0);
-        if (touch.phase == TouchPhase.Began)
+        List<ARRaycastHit> hits = new();
+        if (raycastManager.Raycast(inputPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
         {
-            List<ARRaycastHit> hits = new();
-            if (raycastManager.Raycast(touch.position, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
-            {
-                Pose hitPose = hits[0].pose;
-                Instantiate(prefab, hitPose.position, hitPose.rotation);
-                placed = true;
-            }
+            Pose hitPose = hits[0].pose;
+
+            Instantiate(prefab, hitPose.position, hitPose.rotation);
+
+            placed = true;
         }
     }
+
 }
 
